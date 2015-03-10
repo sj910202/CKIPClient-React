f2382f60ea66b5b312f7926db0f2f97d1515e0b7
kslab <kslab@kslabiMac.local>
2015-03-10 17:12:22 +0800

    Add TCP socket

--- components/loader.js
+++ components/loader.js
@@ -1,8 +1,4 @@
     /** @jsx React.DOM */
-    React.renderComponent(
-        <h1>Hello, world!</h1>,
-        document.getElementById('myDiv')
-    );
 
 
 
@@ -22,12 +18,28 @@
     var MyForm = React.createClass({
 
         getInitialState: function() {
-            return {value: 'This is a description!'};
+            return {value: ''};
         },
-        
+        handleChange: function(event){
+            this.setState({value:event.target.value});
+        },
+
+
+        handleSubmit: function(event){
+            event.preventDefault();
+            console.log(this);
+            alert('I am submit' + this.state.value);
+        },
+
         render: function() {
             var value = this.state.value;
-            return <textarea name="description" value={value}  onChange={this.handleChange} />;
+            return (
+                <form className="MyForm"  textvalue="" onSubmit={this.handleSubmit}>
+                    <h1>MyForm</h1>
+                    <textarea placeholder="Please insert words inside" name="description" value={value}  onChange={this.handleChange} />
+                    <input type="submit" value="post"></input>
+                </form>
+            );
         }
 
     });
@@ -36,7 +48,8 @@
 
 
 
+
     React.renderComponent(
-        <MyComponent/>,
+        <MyForm/>,
         document.getElementById('myDiv')
     );
