bf42f8ac42b4dff236593ea74f7e00236d2e9b66
kslab <kslab@kslabiMac.local>
2015-03-13 18:26:54 +0800

    combine connectsocket and server.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var React = require('react'),
-    DOM = React.DOM,
-    div = DOM.div, button = DOM.button, ul = DOM.ul, li = DOM.li
+    DOM = React.DOM, div = DOM.div, button = DOM.button, ul = DOM.ul, li = DOM.li, textarea = DOM.textarea;
+var CKIPsocket = require('./components/connectsocket');
 
-// This is just a simple example of a component that can be rendered on both 
-// the server and browser 
+
+// This is just a simple example of a component that can be rendered on both
+// the server and browser
 
 module.exports = React.createClass({
 
@@ -11,7 +12,7 @@ module.exports = React.createClass({
     // was first rendered. We also want the button to be disabled until the
     // component has fully mounted on the DOM
     getInitialState: function() {
-        return {items: this.props.items, disabled: true}
+        return {items: this.props.items, disabled: true, value:""}
     },
 
     // Once the component has been mounted, we can enable the button
@@ -19,10 +20,23 @@ module.exports = React.createClass({
         this.setState({disabled: false})
     },
 
-    // Then we just update the state whenever its clicked - but you could imagine
-    // this being updated with the results of AJAX calls, etc
+    // Then we just update the state whenever its clicked by adding a new item to
+    // the list - but you could imagine this being updated with the results of
+    // AJAX calls, etc
     handleClick: function() {
-        this.setState({items: this.state.items.concat(this.state.items.length)})
+        this.setState({
+            items: this.state.items.concat('Item ' + this.state.items.length)
+        })
+    },
+
+    handleTextareaChange: function(event){
+        console.log(event.target.value);
+        this.setState({value:event.target.value});
+    },
+
+    handleSubmit: function(){
+        CKIPsocket(this.state.value);
+        console.log(this.state.value);
     },
 
     // For ease of illustration, we just use the React JS methods directly
@@ -30,11 +44,19 @@ module.exports = React.createClass({
     // Note that we allow the button to be disabled initially, and then enable it
     // when everything has loaded
     render: function() {
+        var value = this.state.value;
         return div(null,
+
             button({onClick: this.handleClick, disabled: this.state.disabled}, 'Add Item'),
+
             ul({children: this.state.items.map(function(item) {
                 return li(null, item)
-            })})
+            })}),
+
+            textarea({placeholder:"請輸入７０字字元！", name:"submitText" ,value:value, onChange:this.handleTextareaChange}),
+            button({onClick: this.handleSubmit(), disabled:this.state.disabled}, "送出")
+
         )
     },
 })
+
---
+++ connectsocket.js
@@ -0,0 +1,91 @@
+var net = require('net');
+var Iconv  = require('iconv').Iconv;
+var fs = require('fs');
+var parseString = require('xml2js').parseString;
+
+
+var HOST = '140.109.19.104';
+var PORT = 1501;
+
+var client = new net.Socket();
+
+module.exports = {
+
+    SubmitText: function(insertString){
+
+        client.connect(PORT, HOST, function() {
+            console.log(xmldataformat(insertString));
+            console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+            // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
+
+            client.write(xmldataformat(insertString));
+        });
+
+
+    }
+
+}
+
+
+function xmldataformat(insertString){
+    //    var insertString = " 中華民國成立後，即將清朝的「學部」改名為「教育部」，當時底下有「承政廳」、「普通教育司」、「專門教育司」及「社會教育司」，從事國內教育興革相關工作。至1926年3月，國民政府於廣州成立後，再由「教育行政委員會」接管相關任務。";
+
+    var xmldata = '<?xml version="1.0" encoding="Big5"?>' +
+            '<wordsegmentation version="0.1">' +
+            '<option showcategory="1" />' +
+            '<authentication username="sj910202" password="sj1011" />' +
+            '<text>'+insertString+'</text>' +
+            '</wordsegmentation>';
+
+    var iconv = new Iconv('utf-8','Big5');
+
+    return iconv.convert(xmldata);
+}
+
+
+client.connect(PORT, HOST, function() {
+    console.log(xmldataformat());
+    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+    // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client 
+
+    client.write(xmldataformat(),function(data){
+        if(data){
+            console.log("True");
+        }else{
+            console.log('False');
+        }
+    });
+
+});
+
+
+// Add a 'data' event handler for the client socket
+// data is what the server sent to this socket
+client.on('data', function(data) {
+    var iconv = new Iconv('Big5','utf-8');
+    var finaldata =  iconv.convert(data);
+    //translate XML to JS object
+    parseString(finaldata, function (err, result) {
+        console.dir(result.wordsegmentation.result[0].sentence[0]);
+        var regexp = new RegExp(/\S/);
+        console.log(regexp.exec(result.wordsegmentation.result));
+    });
+
+    console.log('DATA: ' + finaldata);
+    // Close the client socket completely
+    fs.writeFile("./xmldata",finaldata,function(err){
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+        }
+    });
+
+    client.destroy();
+
+});
+
+// Add a 'close' event handler for the client socket
+client.on('close', function() {
+    console.log('Connection closed');
+});
